b1c6e6a6cef307f58b90085129f4031fd2943368
Timothy Appnel <tim@appnel.com>
2010-09-28 17:49:28 -0400

    Initial commit with WP DePo Masthead theme source from http://svn.automattic.com/wpcom-themes/depo-masthead/ to use as a reference.

---
+++ wpref/footer.php
@@ -0,0 +1,10 @@
+	<div id="footer">
+		<p>
+			<a href="http://wordpress.com/" rel="generator">Get a free blog at WordPress.com</a> <?php printf( __( 'Theme: %1$s by %2$s.', 'depo-masthead' ), '<strong>DePo Masthead</strong>', '<a href="http://powazek.com" rel="designer">Derek Powazek</a>' ); ?>
+		</p>
+	</div>
+</div>
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ wpref/searchform.php
@@ -0,0 +1,7 @@
+
+
+<form id="searchform" name="searchform" method="get" action="/?s=">
+<input type="text" name="s" value=""  />
+<input type="submit" id="searchsubmit" style="display: none;" value="<?php esc_attr_e( 'Search', 'depo-masthead' ); ?>" />
+</form>
+
